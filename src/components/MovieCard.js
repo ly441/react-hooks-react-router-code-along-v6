361373eb8bf075aeaf98d3885ec7000b9e0abcb2
Lynn <koliilynn@gmail.com>
2025-04-18 22:43:53 +0300

    add routes and MovieCard component for navigation

---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MovieCard({ movie }) {
+  return (
+    <div className="movie-card">
+      <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+    </div>
+  );
+}
+
+export default MovieCard;
